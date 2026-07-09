4fb2f993bd56464f11e4130445b6d15c2b85005f
Grace Suh <suhjimin040920@gmail.com>
2026-07-09 14:12:44 +0900

    stack implementation usng Python

---
+++ stack.py
@@ -0,0 +1,44 @@
+class Stack:
+    def __init__(self):
+        # Prepare list to store data 
+        self.items = []
+    
+    '''
+    Add value in the stack 
+    O(1) -> Insert new value at the end of stack, no need to shift other element in the stack 
+    '''
+    def push(self, val):
+        self.items.append(val)
+    
+    '''
+    Delete/pop the top value in the stack 
+    O(1) -> Remove a value from the end of stack, no need to shift other element in the stack 
+    '''
+    def pop(self):
+        try:
+            return self.items.pop()
+        except IndexError:
+            print('Stack is empty')
+    
+    '''
+    Return the top value in the stack 
+    O(1) -> We can directly access to the value that is on top of the stack 
+    '''
+    def top(self):
+        try:
+            return self.items[-1]
+        except IndexError:
+            print('Stack is empty')
+    
+    '''
+    Return how many items in the stack 
+    O(1) 
+    '''
+    def __len__(self):
+        return len(self.items)
+
+S = Stack()
+S.push(10)
+S.push(2)
+print(S.pop())
+print(S.top())
